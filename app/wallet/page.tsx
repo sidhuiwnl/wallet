25b73e717c2b209be84626ac1a83a37f10059542
Sidharth <sidharthinfernal@gmail.com>
2024-08-16 23:40:24 +0530

    added dark-mode option

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,5 +1,34 @@
+import { generateMnemonic } from "bip39";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
+const mnemoic = generateMnemonic();
+
+
 export default function Wallet(){
+    
+   
     return(
-        <h1>Wallet</h1>
+        <div className="w-screen h-screen ">
+             <Navbar/>
+            <div className="text-center">
+                <Button>Generate Secret </Button>
+            </div>
+        </div>
+        
+    )
+}
+
+
+function Navbar(){
+    return(
+        <div className="p-4 flex justify-between items-center">
+            <div>
+                <h1 className="md:text-6xl font-extrabold darK:text-white">CRYWO</h1>
+            </div>
+            <div>
+                <ModeToggle/>
+            </div>
+        </div>
     )
 }
